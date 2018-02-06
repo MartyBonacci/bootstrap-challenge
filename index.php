c113f4c06e15006f2f8e55c96b411c72d6f6885c
Marty Bonacci <martybonacci@gmail.com>
2018-02-06 12:38:42 -0700

    finished bootstrap challenge

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+				integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+				  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+				  crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+				  integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+				  crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+				  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+				  crossorigin="anonymous"></script>
+
+		<meta name="author" content="Marty Bonacci">
+		<meta name="description" content="Bootstrap Challenge">
+		<title>Bootstrap Challenge</title>
+<!--		sklfjasdefhsdjgfhksd-->
+	</head>
+
+	<body>
+		<div class="container">
+
+			<nav class="mt-md-3 navbar navbar-expand-lg navbar-light bg-light rounded border ">
+					<span class="navbar-nav">
+					<a class="navbar-brand nav-link" href="#">Title</a>
+					</span>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+						  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<span class=" mr-auto">
+					</span>
+					<ul class="navbar-nav ">
+						<li>
+							<a class="nav-link" href="#">Link1</a>
+						</li>
+						<li>
+							<a class="nav-link" href="#">Link2</a>
+						</li>
+						<li>
+							<a class="nav-link" href="#">Link3</a>
+						</li>
+						<li><a class="nav-link" href="#">Link4</a></li>
+					</ul>
+				</div>
+			</nav>
+
+
+			<div class="jumbotron my-5">
+				<h1 class="display-4">Hello, world!</h1>
+				<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra
+					attention to featured content or information.</p>
+				<p class="lead">
+					<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+				</p>
+			</div>
+
+		</div> <!--container-->
+		<div class="container-fluid my-5">
+
+			<div class="row bg-light text-dark">
+				<div class="container px-0">
+					<div class="col-md-3">
+						<img class="float-left img-fluid py-5" src="chara-square-offset.png" alt="Chara">
+					</div>
+
+					<div class="col-md-9 float-right py-5">
+						<p class="card-text">Motorcycle collection clubbing wildly attractive doesn't hurt that just proves my
+							point. I am a hoarder, but only of top shelf stuff really only soft drugs staying up late I did a
+							lot of modeling work in the mid-80s finishing my novel, someone to provide for you because I am a
+							paradox if you like for real though don't waste my time. Throwing rocks at trains extreme my
+							hobbies include my last partner told me keep up with me my deep, manly voice.</p>
+						<p>You could say I'm old-fashioned I may be somewhat jaded no crazy chicks ask your mother. Keep up
+							with me I'm a nice guy ultramarathons most cats eventually love me that's what she said, I enjoy my
+							hobbies include heyyy are you really going to rule me out becausae of it? I starred in my own
+							reality show. Extreme that just proves my point beekeeping but I only smoke when drinking my wife
+							that means I am wonderful.</p>
+					</div>
+				</div><!--container-->
+			</div>
+		</div><!--container-fluid-->
+		<div class="container">
+			<div class="row my-5">
+
+				<div class="col-md-3">
+					<img class="img-fluid rounded-circle my-2" src="chara-square-offset.png" alt="rounded-circle image">
+					<div class="card my-2">
+						<div class="card-body bg-light">
+							<p class="card-text">Pics on request because I am a paradox playing devil's advocate Libertarian. I
+								am extremely experienced and talented when I get drunk I should have grown up in the 40s my
+								hobbies
+								include work hard play hard, in my birthday suit unworthy of serious consideration
+								ultramarathons.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3">
+					<img class="img-fluid rounded-circle my-2" src="chara-square-offset.png" alt="rounded-circle image">
+					<div class="card my-2">
+						<div class="card-body bg-light">
+							<p class="card-text">Pics on request because I am a paradox playing devil's advocate Libertarian. I
+								am extremely experienced and talented when I get drunk I should have grown up in the 40s my
+								hobbies
+								include work hard play hard, in my birthday suit unworthy of serious consideration
+								ultramarathons.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<img class="img-fluid rounded-circle my-2" src="chara-square-offset.png" alt="rounded-circle image">
+					<div class="card my-2">
+						<div class="card-body bg-light">
+							<p class="card-text">Pics on request because I am a paradox playing devil's advocate Libertarian. I
+								am extremely experienced and talented when I get drunk I should have grown up in the 40s my
+								hobbies
+								include work hard play hard, in my birthday suit unworthy of serious consideration
+								ultramarathons.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<img class="img-fluid rounded-circle my-2" src="chara-square-offset.png" alt="rounded-circle image">
+					<div class="card my-2">
+						<div class="card-body bg-light">
+							<p class="card-text">Pics on request because I am a paradox playing devil's advocate Libertarian. I
+								am extremely experienced and talented when I get drunk I should have grown up in the 40s my
+								hobbies
+								include work hard play hard, in my birthday suit unworthy of serious consideration
+								ultramarathons.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--container-->
+
+
+		<footer>
+			<div class="container-fluid bg-dark text-light mt-3 py-4">
+				<div class="text-center">
+					bla bla bla bla bla bla bla bla
+				</div>
+			</div>
+		</footer>
+
+
+	</body>
+
+</html>
